aad20596258f0bbe8e236f157ec192504b8a4045
jeremihz <mihjere68@gmail.com>
2019-08-26 11:53:15 +0300

    finishing with db functions

---
+++ orderSuccess.php
@@ -0,0 +1,131 @@
+﻿<?php
+if(!isset($_REQUEST['id'])){
+    header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Target Furniture</title>
+<!-- for-mobile-apps -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- FlexSlider -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<script defer src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+						$(window).load(function(){
+						  $('.flexslider').flexslider({
+							animation: "slide",
+							start: function(slider){
+							  $('body').removeClass('loading');
+							}
+						  });
+						});
+					  </script>
+<!-- //FlexSlider -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+	
+<body>
+<!-- banner -->
+	<div class="banner">
+		<div class="container">
+		<div class="search-bar">
+				<form action="search.php" method="post">
+				<input type="text">
+				<input type="submit" name="user_query"" value="Search" />
+				</form>
+			</div>
+			<div class="header-nav">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="logo">
+							<a class="navbar-brand" href="index.html">Target Furniture<span>Zungumza Nasi</span></a>
+						</div>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav cl-effect-14">
+							<li class="active"><a href="index.html" class="active">Home</a></li>
+                            <li><a href="index.php">Sign In</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="store.html">what we have</a></li>
+							<li><a href="contact.html">Contact Us</a></li>
+						</ul>
+						
+					</div><!-- /.navbar-collapse -->	
+				</nav>
+			</div>
+			<br><br>
+			<div class="container">
+			<h1>Order Status</h1>
+			<p>Your order has submitted successfully.<?php echo $_GET['id']; ?></p>
+			</div>
+			<div class="banner-info-grids">
+				
+				<div class="clearfix"></div>
+				<div class="banner-info-pos">
+					<a href="#timings" class="scroll"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+<!--footer-->
+	
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">		
+			<p>© 2018 www.targetfurniture.com <a href="#"></a></p>					
+		</div>
+	</div>
+<!--//footer-->
+<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+</body>
+</html>
---
+++ viewCart.php
@@ -0,0 +1,173 @@
+﻿<?php
+
+include 'Cart.php';
+$cart = new Cart;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Target Furniture</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- FlexSlider -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<script defer src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+						$(window).load(function(){
+						  $('.flexslider').flexslider({
+							animation: "slide",
+							start: function(slider){
+							  $('body').removeClass('loading');
+							}
+						  });
+						});
+					  </script>
+<!-- //FlexSlider -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+	
+<body>
+<!-- banner -->
+	<div class="banner">
+		<div class="container">
+		<div class="search-bar">
+				<form action="search.php" method="post">
+				<input type="text">
+				<input type="submit" name="user_query"" value="Search" />
+				</form>
+			</div>
+			<div class="header-nav">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="logo">
+							<a class="navbar-brand" href="index.html">Target furniture <span>Zungumza Nasi</span></a>
+						</div>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav cl-effect-14">
+							<li class="active"><a href="index.html" class="active">Home</a></li>
+                            <li><a href="index.php">Sign In</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="store.html">Store</a></li>
+							<li><a href="contact.html">Contact Us</a></li>
+						</ul>
+						
+					</div><!-- /.navbar-collapse -->	
+				</nav>
+			</div>
+			<div class="banner-info-grids">
+				<div class="col-md-6 banner-info">
+					<h1>We Provide high quality <span>wooden furnitures</span></h1>
+				</div>
+				
+				<div class="clearfix"></div>
+				<div class="banner-info-pos">
+					<a href="#timings" class="scroll"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+<div class="container">
+    <h1>Shopping Cart</h1>
+    <table class="table">
+    <thead>
+        <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        if($cart->total_items() > 0){
+            
+            $cartItems = $cart->contents();
+            foreach($cartItems as $item){
+        ?>
+        <tr>
+            <td><?php echo $item["name"]; ?></td>
+            <td><?php echo 'ksh '.$item["price"].' Price'; ?></td>
+            <td><input type="number" class="form-control text-center" value="<?php echo $item["qty"]; ?>" onchange="updateCartItem(this, '<?php echo $item["rowid"]; ?>')"></td>
+            <td><?php echo 'ksh '.$item["subtotal"].' Price'; ?></td>
+            <td>
+           
+                <a href="cartAction.php?action=removeCartItem&id=<?php echo $item["rowid"]; ?>" class="btn btn-danger" onclick="return confirm('Are you sure?')"><i class="glyphicon glyphicon-trash"></i></a>
+            </td>
+        </tr>
+        <?php } }else{ ?>
+        <tr><td colspan="5"><p>Your cart is empty.....</p></td>
+        <?php } ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td><a href="cardadd.php" class="btn btn-warning"><i class="glyphicon glyphicon-menu-left"></i> Continue Shopping</a></td>
+            <td colspan="2"></td>
+            <?php if($cart->total_items() > 0){ ?>
+            <td class="text-center"><strong>Total <?php echo 'ksh '.$cart->total().' Price'; ?></strong></td>
+            <td><a href="checkout.php" class="btn btn-success btn-block">Checkout <i class="glyphicon glyphicon-menu-right"></i></a></td>
+            <?php } ?>
+        </tr>
+    </tfoot>
+    </table>
+</div>
+<!--footer-->
+	
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">		
+			<p>© 2018 www.targetfurniture.com <a href="#"></a></p>					
+		</div>
+	</div>
+<!--//footer-->
+<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+</body>
+</html>
